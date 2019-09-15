3510729e8822a1c6099d1142a4972862714ba61c
Maimell <malinka77@gmail.com>
2019-09-14 23:38:09 -0700

    added timer, the game is ready, well, almost, maybe tomorrow will add some sounds

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Harry Potter TriviaGame
 
-How well do you know Harry Potter books?
+**How well do you know Harry Potter world?**
 
-You will be given 15 seconds to answer each question.
+From spells to ghosts, potions to professors, wands to witches, discover just how much you know about the beloved wizarding world!
+
+
+**Here's how the app works:**
+
+*   You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+
+*   The scenario is similar for wrong answers and time-outs.
+
+
+*   If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+
+
+*   On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
+[Playable Demo](https://malinkamell.github.io/TriviaGame/)
+
+**Optimized for 1920x1080 screen resolution and Google Chrome Version 77.0.3865.75**
+
+_Made for UOA Bootcamp, September 2019_
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,186 +1,285 @@
 $(document).ready(function () {
 
-//declaring reusable class object in order to reuse it and create as many questions as i want instead of creatiing directly an array and modify it every time
-class questionObject {
-    constructor(question, answerOptions, rightAnswer, imgFile) {
-        this.question = question,
-        this.answerOptions = answerOptions,
-        this.rightAnswer = rightAnswer,
-        this.imgFile = imgFile
+    //declaring reusable class object in order to reuse it and create as many questions as i want instead of creatiing directly an array and modify it every time
+    class questionObject {
+        constructor(question, answerOptions, rightAnswer, imgFile) {
+            this.question = question,
+                this.answerOptions = answerOptions,
+                this.rightAnswer = rightAnswer,
+                this.imgFile = imgFile
+        }
     }
-}
-//declaring an array and going to push here newly created questions 
-let arrayOfQuestions = [];
 
-//question 1: declaring and pushing
-const questionOne = new questionObject(
-    "In the books, where did we see Severus Snape for the first time?", 
-    ["In the Great Hall", "In a Hogward Corridor", "In Potions Classroom", "On Hogwards Express"],
-    "In the Great Hall",
-    "greatHall.gif"
+    //declaring an array and going to push here newly created questions 
+    let arrayOfQuestions = [];
+
+    //question 1: declaring and pushing
+    const questionOne = new questionObject(
+        "In the books, where did we see Severus Snape for the first time?",
+        ["In the Great Hall", "In a Hogward Corridor", "In Potions Classroom", "On Hogwards Express"],
+        "In the Great Hall",
+        "greatHall.gif"
     );
-arrayOfQuestions.push(questionOne);
+    arrayOfQuestions.push(questionOne);
 
-//question 2: declaring and pushing
-const questionTwo = new questionObject(
-    "What is the name of Luna Lovegoods’s father?",
-    ["Xenophilius", "Amos", "Dedalus", "Aberforth"],
-    "Xenophilius",
-    "lovegoods.gif"
-);
-arrayOfQuestions.push(questionTwo);
+    //question 2: declaring and pushing
+    const questionTwo = new questionObject(
+        "What is the name of Luna Lovegoods’s father?",
+        ["Xenophilius", "Amos", "Dedalus", "Aberforth"],
+        "Xenophilius",
+        "lovegoods.gif"
+    );
+    arrayOfQuestions.push(questionTwo);
 
-//question 3: declaring and pushing
-const questionThree = new questionObject(
-    "In which book is Sirius Black first mentioned?",
-    ["Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Half-Blood Prince"],
-    "Harry Potter and the Sorcerer's Stone",
-    "syrius.gif"
-);
-arrayOfQuestions.push(questionThree);
+    //question 3: declaring and pushing
+    const questionThree = new questionObject(
+        "In which book is Sirius Black first mentioned?",
+        ["Harry Potter and the Sorcerer's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Half-Blood Prince"],
+        "Harry Potter and the Sorcerer's Stone",
+        "syrius.gif"
+    );
+    arrayOfQuestions.push(questionThree);
 
-//question 4: declaring and pushing
-const questionFour = new questionObject(
-    "What is Albus Dumbledore’s full name?",
-    ["Albus Percival Wulfric Brian Dumbledore", "Albus Peter Wolfgan Bjorn Dumbledore", "Albus Potter Waltan Bruce Dumbledore", "Albus Phineas Wilhelm Bane Dumbledore"],
-    "Albus Percival Wulfric Brian Dumbledore",
-    "dumbledore.gif"
-);
-arrayOfQuestions.push(questionFour);
+    //question 4: declaring and pushing
+    const questionFour = new questionObject(
+        "What is Albus Dumbledore’s full name?",
+        ["Albus Percival Wulfric Brian Dumbledore", "Albus Peter Wolfgan Bjorn Dumbledore", "Albus Potter Waltan Bruce Dumbledore", "Albus Phineas Wilhelm Bane Dumbledore"],
+        "Albus Percival Wulfric Brian Dumbledore",
+        "dumbledore.gif"
+    );
+    arrayOfQuestions.push(questionFour);
 
-//question 5: declaring and pushing
-const questionFive = new questionObject(
-    "What item of clothing grants Dobby his freedom?",
-    ["A slimy sock", "A dirty glove", "A broken shoe", "An old hat"],
-    "A slimy sock",
-    "dobby.gif"
-);
-arrayOfQuestions.push(questionFive);
+    //question 5: declaring and pushing
+    const questionFive = new questionObject(
+        "What item of clothing grants Dobby his freedom?",
+        ["A slimy sock", "A dirty glove", "A broken shoe", "An old hat"],
+        "A slimy sock",
+        "dobby.gif"
+    );
+    arrayOfQuestions.push(questionFive);
 
-//question 6: declaring and pushing
-const questionSix = new questionObject(
-    "In which book does Harry NOT travel on the Hogwarts Express?",
-    ["Harry Potter and the Deathly Hallows", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Prisoner of Azkaban"],
-    "Harry Potter and the Deathly Hallows",
-    "hogwards_express.gif"
-);
-arrayOfQuestions.push(questionSix);
+    //question 6: declaring and pushing
+    const questionSix = new questionObject(
+        "In which book does Harry NOT travel on the Hogwarts Express?",
+        ["Harry Potter and the Deathly Hallows", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Prisoner of Azkaban"],
+        "Harry Potter and the Deathly Hallows",
+        "hogwards_express.gif"
+    );
+    arrayOfQuestions.push(questionSix);
 
-//question 7: declaring and pushing
-const questionSeven = new questionObject(
-    "What did Voldemort say was the most powerfully magical number?",
-    ["7", "666", "13", "3"],
-    "7",
-    "voldemort.gif"
-);
-arrayOfQuestions.push(questionSeven);
+    //question 7: declaring and pushing
+    const questionSeven = new questionObject(
+        "What did Voldemort say was the most powerfully magical number?",
+        ["7", "666", "13", "3"],
+        "7",
+        "voldemort.gif"
+    );
+    arrayOfQuestions.push(questionSeven);
 
-//question 8: declaring and pushing
-const questionEight = new questionObject(
-    "Which book did Hagrid assign to his Care of Magical Creatures class?",
-    ["The Monster Book of Monsters", "Fantastic Beasts and Where To Find Them", "Fowl or Foul?: A Study of Hippogriff Brutality", "Hairy Snout, Human Heart"],
-    "The Monster Book of Monsters",
-    "monter_book.gif"
-);
-arrayOfQuestions.push(questionEight);
+    //question 8: declaring and pushing
+    const questionEight = new questionObject(
+        "Which book did Hagrid assign to his Care of Magical Creatures class?",
+        ["The Monster Book of Monsters", "Fantastic Beasts and Where To Find Them", "Fowl or Foul?: A Study of Hippogriff Brutality", "Hairy Snout, Human Heart"],
+        "The Monster Book of Monsters",
+        "monter_book.gif"
+    );
+    arrayOfQuestions.push(questionEight);
 
-//question 9: declaring and pushing
-const questionNine = new questionObject(
-    "Which pub provides a secret passage to Hogwarts?",
-    ["The Hog's Head", "The Three Broomsticks", "The Leaky Cauldron", "Madam Puddifoot's"],
-    "The Hog's Head",
-    "ariana.gif"
-);
-arrayOfQuestions.push(questionNine);
+    //question 9: declaring and pushing
+    const questionNine = new questionObject(
+        "Which pub provides a secret passage to Hogwarts?",
+        ["The Hog's Head", "The Three Broomsticks", "The Leaky Cauldron", "Madam Puddifoot's"],
+        "The Hog's Head",
+        "ariana.gif"
+    );
+    arrayOfQuestions.push(questionNine);
 
-//question 10: declaring and pushing
-const questionTen = new questionObject(
-    "What broomstick did the Weasley twins use playing Quidditch for Gryffindor?",
-    ["Cleansweep Five", "Thunderbolt VII", "Nimbus 2000", "Comet Two Ninety"],
-    "Cleansweep Five",
-    "quiddich.gif"
-);
-arrayOfQuestions.push(questionTen);
+    //question 10: declaring and pushing
+    const questionTen = new questionObject(
+        "What broomstick did the Weasley twins use playing Quidditch for Gryffindor?",
+        ["Cleansweep Five", "Thunderbolt VII", "Nimbus 2000", "Comet Two Ninety"],
+        "Cleansweep Five",
+        "quiddich.gif"
+    );
+    arrayOfQuestions.push(questionTen);
 
-//setting question number to 0
-let questionNumber = 0;
+    //setting question number to 0
+    let questionNumber = 0;
+    //switch for next question
+    let questionSwitch = false;
+    //counting right answers
+    let rightAnswers = 0;
+    //counting wrong answers
+    let wrongAnswers = 0;
+    //non answers
+    let noAnswers = 0;
 
-//function for shufling arrays
-function shuffle(array) {
-    array.sort(() => Math.random() - 0.5);
-  }
-
-
-let showRightAnswerWindow = () => {
-    $("#questionDiv").attr("style", "display:none");
-    $("#answers_div").attr("style", "display:none");
-    $("#rightAnswerDiv").attr("style", "display:block");
-    $("#correct").attr("style", "display:block");
-    $("#correct_answer").attr("style", "display:block");
-    $("#img_div").attr("style", "display:block");
-    $("#div_correct_answer").attr("style", "display:block");
-    $("#correct").text("That's correct!");
-};
-
-let showWrongAnswerWindow = () => {
-    $("#questionDiv").attr("style", "display:none");
-    $("#answers_div").attr("style", "display:none");
-    $("#rightAnswerDiv").attr("style", "display:block");
-    $("#correct").attr("style", "display:block");
-    $("#correct_answer").attr("style", "display:block");
-    $("#img_div").attr("style", "display:block");
-    $("#div_incorrect_answer").attr("style", "display:block");
-    $("#correct").text("Incorrect!");
-}
+    //Timer functionality
 
 
-//start game function
-let startGame = (param) => {
+    //  Variable that will hold our setInterval that runs the stopwatch
+    let intervalId;
 
-    //shuffling the questions array so they don't show in the same order every time
-    shuffle(arrayOfQuestions);
+    // prevents the clock from being sped up unnecessarily
+    let clockRunning = false;
+    let time = 15;
 
-    //shuffling the answers array so they don't show in the same order every time
-    shuffle(param[questionNumber].answerOptions);
+    function timerStop() {
+        clearInterval(intervalId);
+        clockRunning = false;
+        console.log("stopping")
+    }
 
-    //appending questions and answers to html
-    $("#question").text(param[questionNumber].question);
-    $("#answer1").val(param[questionNumber].answerOptions[0]);
-    $("#answer2").val(param[questionNumber].answerOptions[1]);
-    $("#answer3").val(param[questionNumber].answerOptions[2]);
-    $("#answer4").val(param[questionNumber].answerOptions[3]);
-    $("#label_1").text(param[questionNumber].answerOptions[0]);
-    $("#label_2").text(param[questionNumber].answerOptions[1]);
-    $("#label_3").text(param[questionNumber].answerOptions[2]);
-    $("#label_4").text(param[questionNumber].answerOptions[3]);
-    
-    //checking the value of clicked radio
-    $("input[type='radio']").click(function(){
-        let radioValue = $("input[name='answer']:checked").val();
-        if(radioValue === param[questionNumber].rightAnswer){
-            $("#correct_answer").text(param[questionNumber].rightAnswer);
-            $("#image_file").attr("src", "assets/images/gifs/" + param[questionNumber].imgFile);
-            console.log("Right Answer! " + radioValue);
-            questionNumber++;
-            console.log(questionNumber);
-            showRightAnswerWindow();
+    function timerStart() {
+        if (!clockRunning) {
+            intervalId = setInterval(timerCount, 1000);
+            clockRunning = true;
+        }
+    }
+
+    function timerCount() {
+        if (time > 0) {
+            time--;
+            let displayTime = "0" + time;
+            $("#timer").html(`Time remaining: ${displayTime}`);
+        }
+        if (time === 0) {
+            let theAnswer = arrayOfQuestions[questionNumber].rightAnswer;
+            let image = arrayOfQuestions[questionNumber].imgFile;
+            timerStop();
+            time = 0;
+            $("#timer").text("");
+            $("#div_correct_answer").show();
+            $("#div_correct_answer").html(`Time's up!!<br> <small>The right answer was: </small>`);
+            $("#answers_div").html(`<h4> ${theAnswer}</h4>`);
+            $("#image_file").attr("src", `assets/images/gifs/${image}`);
+            questionSwitch = true;
+            noAnswers++;
+            showAnswerWindow();
+        }
+    }
+
+    //function for shufling array
+    function shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
+    //reset game interface
+    let resetWindow = () => {
+        $("#questionDiv").show();
+        $("#answers_div").show();
+        $("#answers_div").html("");
+        $("#rightAnswerDiv").hide();
+        $("#img_div").hide();
+        createGameInterface();
+    }
+    //showing right answer
+    let showAnswerWindow = () => {
+        $(".answer").remove();
+        $("#questionDiv").hide();
+        $("#rightAnswerDiv").show();
+        $("#img_div").show();
+        setTimeout(function () {
+            console.log("waiting 3 seconds")
+            resetWindow();
+        }, 3000)
+    }
+    //game over, restart window
+    let restartWindow = () => {
+
+
+
+        timerStop();
+        time = 0;
+        $("#timer").html(`<button class="restart-button font-weight-bold image-button">Restart</button>`);
+        $("#question").html("");
+        $('#answers_div').html(`
+        <h2>Game Over!</h2>
+        <p>Right answers: <strong>${rightAnswers}</strong> </p>
+        <p>Wrong answers: <strong>${wrongAnswers}</strong></p>
+        <p>Unanswered questions: <strong>${noAnswers}</strong></p>
+        `);
+
+    }
+
+
+    //start game function
+
+    let createGameInterface = () => {
+        $(".restart-button").hide();
+        $(".start-button").hide();
+        if (questionNumber < arrayOfQuestions.length) {
+            if (questionSwitch === true) {
+                questionNumber++;
+            }
+
+            //shuffling the answers array so they don't show in the same order every time
+            shuffle(arrayOfQuestions[questionNumber].answerOptions);
+
+            //appending questions and answers to html
+            $("#question").text(arrayOfQuestions[questionNumber].question);
+            $.each(arrayOfQuestions[questionNumber].answerOptions, function (index, key) {
+                $('#answers_div').append($(`<button class="answer">${key}</button>`));
+            })
+            //timerReset();
+            time = 10;
+            timerStart();
+            console.log(arrayOfQuestions[questionNumber].question)
+
+        }
+        //if no more question show game over window
+        if (questionNumber === arrayOfQuestions.length - 1) {
+            timerStop();
+            time = 0;
+            $("#timer").text("");
+            restartWindow();
+        }
+
+    }
+
+
+    $("body").on("click", ".answer", function () {
+
+        let rightAnswer = arrayOfQuestions[questionNumber].rightAnswer;
+        if ($(this).text() === rightAnswer) {
+            timerStop();
+            time = 0;
+            $("#timer").text("");
+            $("#div_correct_answer").show();
+            $("#div_correct_answer").text(`That's correct!`);
+            $("#answers_div").html(`<h4 class="font-weight-bold">${rightAnswer}</h4>`);
+            $("#image_file").attr("src", `assets/images/gifs/${arrayOfQuestions[questionNumber].imgFile}`);
+            questionSwitch = true;
+            rightAnswers++;
+            showAnswerWindow();
         }
         else {
-            $("#right_answer").text("It was: " + param[questionNumber].rightAnswer);
-            $("#image_file").attr("src", "assets/images/gifs/" + param[questionNumber].imgFile);
-            questionNumber++;
-            showWrongAnswerWindow();
+            timerStop();
+            time = 0;
+            $("#timer").text("");
+            $("#div_correct_answer").show();
+            $("#div_correct_answer").html(`Incorrect!<br> <small>The right answer is: </small>`);
+            $("#answers_div").html(`<h4> ${rightAnswer}</h4>`);
+            $("#image_file").attr("src", `assets/images/gifs/${arrayOfQuestions[questionNumber].imgFile}`);
+            questionSwitch = true;
+            wrongAnswers++;
+            showAnswerWindow();
         }
-        
-        
-    }); 
+    });
 
+    $("body").on("click", ".restart-button", function () {
+        questionSwitch = false;
+        questionNumber = 0;
+        resetWindow();
 
-}
+    });
 
+    $("body").on("click", ".start-button", function () {
+        questionSwitch = false;
+        questionNumber = 0;
+        resetWindow();
 
+    });
 
-    
-startGame(arrayOfQuestions)
+    //createGameInterface();
 
 })
